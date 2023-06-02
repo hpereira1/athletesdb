ed76725b656155ccfce65648e2fab2a2e7eec504
hpereira1 <pereirah123@gmail.com>
2023-06-02 20:41:57 -0300

    sql

--- shotput/test.py
+++ shotput/test.py
@@ -1,22 +1,23 @@
 import csv
+import os
 
+directory = '/home/hpereira1/Documents/Projetopet/athletesdb/shotput/data_shotput'
 # Replace these values with your own.
-csv_file_path = 'athletesdb/shotput/data_shotput/OGmen22.csv'
-sql_file_path = 'athletesdb/shotput/data_shotput/test.sql'
-table_name = 'test'
+#csv_file_path = 'athletesdb/shotput/data_shotput/OGmen22.csv'
+table_name = 'shotput'
 field_names = ['competition', 'country', 'link', 'mark', 'name', 'position', 'sport']
-
+sql_script = ''
 # Open the CSV file and read its contents.
-with open(csv_file_path, 'r') as csv_file:
-    reader = csv.reader(csv_file)
-    # Skip the header row.
-    next(reader)
-    # Open the SQL file for writing.
-    with open(sql_file_path, 'w') as sql_file:
-        # Loop through the rows in the CSV file.
-        for row in reader:
-            # Generate an SQL statement for the current row.
-            values = ', '.join("'" + value.replace("'", "''") + "'" for value in row)
-            sql_statement = "INSERT INTO {} ({}) VALUES ({});\n".format(table_name, ', '.join(field_names), values)
-            # Write the SQL statement to the SQL file.
-            sql_file.write(sql_statement)
+for filename in os.listdir(directory):
+    if filename.endswith('.csv'):
+        file_path = os.path.join(directory, filename)
+        with open(file_path, 'r') as csv_file:
+            reader = csv.reader(csv_file)
+            # Skip the header row.
+            next(reader)
+            for row in reader:
+                sql_script += f"INSERT INTO {table_name} (competition, country,gender, link, mark, name, position, sport)  VALUES ('{row[0]}', '{row[1]}', '{row[2]}','{row[3]}',{row[4]},'{row[5]}','{row[6]}', '{row[7]}');\n"
+                # Write the SQL statement to the SQL file.
+with open('insert_data.sql', 'w') as sql_file:
+    sql_file.write(sql_script)
+print("script gerado")
